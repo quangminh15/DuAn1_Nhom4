54c14c9b417545cb26733162611b7ea93678ac3f
quangminh15 <phamm7696@gmail.com>
2022-11-09 14:01:39 +0700

    quangminh

--- Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/DangNhap.java
+++ Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/DangNhap.java
@@ -4,7 +4,15 @@
  */
 package com.nhom4.ui;
 
+import com.nhom4.entity.NhanVien;
+import com.nhom4.utils.Auth;
+import com.nhom4.utils.MsgBox;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.Locale;
+import javax.swing.JPanel;
 
 /**
  *
@@ -20,6 +28,21 @@ public class DangNhap extends javax.swing.JDialog {
         initComponents();
         init();
     }
+    
+    class jPanelGradient extends JPanel {
+
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            int width = getWidth();
+            int height = getHeight();
+
+            Color color1 = new Color(255, 149, 81);
+            Color color2 = new Color(49, 157, 160);
+            GradientPaint gp = new GradientPaint(0, 0, color1, 180, height, color2);
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, width, height);
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -30,65 +53,149 @@ public class DangNhap extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        txtTenDangnhap = new javax.swing.JTextField();
+        jPanel1 = new jPanelGradient();
+        pnlForm = new javax.swing.JPanel();
+        lblTitle = new javax.swing.JLabel();
+        lblMatKhau = new javax.swing.JLabel();
+        txtMaNV = new javax.swing.JTextField();
         txtMatKhau = new javax.swing.JPasswordField();
-        btnOK = new javax.swing.JButton();
-        btnHuy = new javax.swing.JButton();
+        btnKetThuc = new javax.swing.JButton();
+        btnDangNhap = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jLabel1.setText("Tên đăng nhập:");
+        pnlForm.setBackground(new java.awt.Color(255, 255, 255));
+        pnlForm.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setText("Mật khẩu:");
+        lblTitle.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(0, 102, 102));
+        lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitle.setText("ĐĂNG NHẬP");
+        pnlForm.add(lblTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 30, 240, 39));
 
-        btnOK.setText("OK");
+        lblMatKhau.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblMatKhau.setForeground(new java.awt.Color(102, 102, 102));
+        lblMatKhau.setText("Mat Khau");
+        pnlForm.add(lblMatKhau, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, -1, -1));
 
-        btnHuy.setText("Hủy");
+        txtMaNV.setBackground(new java.awt.Color(231, 231, 231));
+        txtMaNV.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        txtMaNV.setForeground(new java.awt.Color(102, 102, 102));
+        txtMaNV.setText("  Ten Tai khoan");
+        txtMaNV.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(229, 229, 229)));
+        txtMaNV.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtMaNVFocusLost(evt);
+            }
+        });
+        txtMaNV.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtMaNVMouseClicked(evt);
+            }
+        });
+        txtMaNV.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtMaNVActionPerformed(evt);
+            }
+        });
+        pnlForm.add(txtMaNV, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, 360, 60));
+
+        txtMatKhau.setBackground(new java.awt.Color(229, 229, 229));
+        txtMatKhau.setForeground(new java.awt.Color(102, 102, 102));
+        txtMatKhau.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(229, 229, 229)));
+        txtMatKhau.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtMatKhauFocusLost(evt);
+            }
+        });
+        txtMatKhau.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtMatKhauMouseClicked(evt);
+            }
+        });
+        pnlForm.add(txtMatKhau, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 360, 64));
+
+        btnKetThuc.setText("Kết thúc");
+        btnKetThuc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKetThucActionPerformed(evt);
+            }
+        });
+        pnlForm.add(btnKetThuc, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 260, 170, 42));
+
+        btnDangNhap.setText("Đăng nhập");
+        btnDangNhap.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDangNhapActionPerformed(evt);
+            }
+        });
+        pnlForm.add(btnDangNhap, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 170, 42));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addComponent(pnlForm, javax.swing.GroupLayout.PREFERRED_SIZE, 940, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(63, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(pnlForm, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(84, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(168, 168, 168)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(86, 86, 86)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtTenDangnhap)
-                            .addComponent(txtMatKhau, javax.swing.GroupLayout.DEFAULT_SIZE, 232, Short.MAX_VALUE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(326, 326, 326)
-                        .addComponent(btnOK)
-                        .addGap(41, 41, 41)
-                        .addComponent(btnHuy)))
-                .addContainerGap(276, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(96, 96, 96)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(txtTenDangnhap, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(93, 93, 93)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(62, 62, 62)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnOK)
-                    .addComponent(btnHuy))
-                .addContainerGap(169, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtMaNVFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtMaNVFocusLost
+        if(txtMaNV.getText().equals("")){
+            txtMaNV.setText("  Ten Tai khoan");
+        }
+    }//GEN-LAST:event_txtMaNVFocusLost
+
+    private void txtMaNVMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtMaNVMouseClicked
+        if(txtMaNV.getText().equals("  Ten Tai khoan")){
+            txtMaNV.setText("");
+        }
+    }//GEN-LAST:event_txtMaNVMouseClicked
+
+    private void txtMaNVActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMaNVActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtMaNVActionPerformed
+
+    private void txtMatKhauFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtMatKhauFocusLost
+        if(txtMatKhau.getText().equals(""))
+        lblMatKhau.setVisible(true);
+        //         else
+        //             lblMatKhau.setVisible(false);
+    }//GEN-LAST:event_txtMatKhauFocusLost
+
+    private void txtMatKhauMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtMatKhauMouseClicked
+        lblMatKhau.setVisible(false);
+    }//GEN-LAST:event_txtMatKhauMouseClicked
+
+    private void btnKetThucActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKetThucActionPerformed
+        this.ketThuc();
+    }//GEN-LAST:event_btnKetThucActionPerformed
+
+    private void btnDangNhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDangNhapActionPerformed
+//        this.dangNhap();
+    }//GEN-LAST:event_btnDangNhapActionPerformed
 
     /**
      * @param args the command line arguments
@@ -135,13 +242,38 @@ public class DangNhap extends javax.swing.JDialog {
     private void init() {
          this.setLocationRelativeTo(null);
     }
+    
+//    void dangNhap() {
+//        String manv = txtMaNV.getText();
+//        String matkhau = new String(txtMatKhau.getPassword());
+//        NhanVien nv = nvDAO.selectById(manv);
+//        if (nv == null) {
+//            MsgBox.alert(this, "Tên đăng nhập không chính xác");
+//        } else {
+//            if (!nv.getMatKhau().equals(matkhau)) {
+//                MsgBox.alert(this, "Mật khẩu không chính xác");
+//            } else {
+//                Auth.user = nv;
+//                MsgBox.alert(this, "Đăng nhập thành công!");
+//                this.dispose();
+//            }
+//        }
+//    }
+
+    void ketThuc() {
+        if (MsgBox.confirm(this, "Bạn có chắc muốn kết thúc ứng dụng?")) {
+            System.exit(0);
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnHuy;
-    private javax.swing.JButton btnOK;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton btnDangNhap;
+    private javax.swing.JButton btnKetThuc;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblMatKhau;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JPanel pnlForm;
+    private javax.swing.JTextField txtMaNV;
     private javax.swing.JPasswordField txtMatKhau;
-    private javax.swing.JTextField txtTenDangnhap;
     // End of variables declaration//GEN-END:variables
 }
