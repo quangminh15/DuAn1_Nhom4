542a8781cef9bbf69c5da571917c886447eaf3d1
quangminh15 <phamm7696@gmail.com>
2022-11-13 08:41:57 +0700

    quangminh

--- Nhom4_QL_ShopBanGiay/src/com/nhom4/dao/SanPhamDAO.java
+++ Nhom4_QL_ShopBanGiay/src/com/nhom4/dao/SanPhamDAO.java
@@ -4,10 +4,49 @@
  */
 package com.nhom4.dao;
 
+import com.nhom4.entity.TaiKhoan;
+import com.nhom4.ui.SanPham;
+import java.util.List;
+
 /**
  *
  * @author ACER
  */
-public class SanPhamDAO {
+public class SanPhamDAO extends MainDAO<SanPham, String>  {
+    final String INSERT_SQL = "INSERT INTO SanPham(MaSP, TenSP, SoLuong, MaNCC, Anh, GhiChu) VALUES (?,?,?,?,?,?)";
+    final String UPDATE_SQL = "UPDATE SanPham SET TenSP = ?, SoLuong= ?, MaNCC =?, Anh =?, GhiChu =? WHERE MaSP=?";
+    final String DELETE_SQL = "DELETE FROM TaiKhoan WHERE MaNV = ?";
+    final String SELECT_ALL_SQL = "SELECT * FROM TaiKhoan";
+    final String SELECT_By_Id_SQL = "SELECT * FROM TaiKhoan WHERE Username = ?";
+    
+    @Override
+    public void insert(SanPham entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(SanPham entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<SanPham> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public SanPham selectById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<SanPham> selectBySql(String sql, Object... args) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
