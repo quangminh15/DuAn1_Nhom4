46db3e934c1d8c4e916f7a6147fbfe1741f6f3ea
LinhVuKg <117135882+LinhVuKg@users.noreply.github.com>
2022-11-29 21:39:20 +0700

    vulinh
    hhh

--- Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/CuaSoChinh.java
+++ Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/CuaSoChinh.java
@@ -661,17 +661,24 @@ public class CuaSoChinh extends javax.swing.JFrame {
 
     public void getFormTK(TaiKhoan tk) {
         System.out.println("5");
-        try {
-            if (tk.getRole() == true) {
-                System.out.println("7");
-                lblAdmin.setText("Nhân viên");
-            } else {
-                System.out.println("6");
-                lblAdmin.setText("Quản lý");
-            }
-        } catch (Exception e) {
-            MsgBox.alert(this, "Không tìm thấy2");
-        }
+//        try {
+//            if (tk.getRole() == true) {
+//                System.out.println("7");
+//                lblAdmin.setText("Nhân viên");
+//            } else {
+//                System.out.println("6");
+//                lblAdmin.setText("Quản lý");
+//            }
+//        } catch (Exception e) {
+//            MsgBox.alert(this, "Không tìm thấy2");
+//        }
+          if(Auth.islogin()){
+              if(Auth.isManager()){
+                  lblAdmin.setText("Quản lý");
+              }else{
+                  lblAdmin.setText("Nhân viên");
+              }
+          }
 
     }
 
