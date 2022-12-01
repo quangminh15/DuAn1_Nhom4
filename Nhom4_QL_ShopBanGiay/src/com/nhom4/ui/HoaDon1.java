cc8288d676e09877bb2dc8749b4ffec8e18b7e73
longhai007 <volonghai1511@gmail.com>
2022-12-01 11:16:15 +0700

    Longhai

--- Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/HoaDon1.java
+++ Nhom4_QL_ShopBanGiay/src/com/nhom4/ui/HoaDon1.java
@@ -47,9 +47,9 @@ public class HoaDon1 extends javax.swing.JPanel {
         fillTableHDCT();
         fillCboMaCT();
         btnSave.setEnabled(false);
-        //btnSaveHDCT.setEnabled(false);
-        //txtDonGia.setEditable(false);
-        //txtThanhTien.setEditable(false);
+        btnSaveHDCT.setEnabled(false);
+        txtDonGia.setEditable(false);
+        txtThanhTien.setEditable(false);
         setStatus(false);
         //setStatusHDCT(false);
         thanhTien();
@@ -250,11 +250,10 @@ public class HoaDon1 extends javax.swing.JPanel {
 
     public void setStatusHDCT(boolean b) {
         int i = tbledark1.getSelectedRow();
-        if (b == false) {
-            if (i < 0) {
+       
+            if (i < 0||b == false) {
                 lblThongBao.setText("CHUA CHON HOA DON");
                 lblThongBao.setVisible(!b);
-
                 lblMaHD.setText("");
                 lblSize.setText("");
                 lblMau.setText("");
@@ -265,8 +264,7 @@ public class HoaDon1 extends javax.swing.JPanel {
                 cboSL.setEnabled(false);
 
                 return;
-            }
-
+            
         } else {
             lblThongBao.setVisible(!b);
             tblHDCT.setEnabled(true);
